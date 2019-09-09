49fa2036c13b2ae4a2ecaa1f85cdae300c8a1a17
marcelo <marartb4@hotmail.com>
2019-09-09 08:09:41 -0700

    Add files via upload

---
+++ ataquenemigolagarto.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ataquenemigolagarto : MonoBehaviour
+{
+
+    int contador = 0;
+    public GameObject orb;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            print("tocamos");
+            Destroy(gameObject);
+
+            Instantiate(orb, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), Quaternion.identity);
+        }
+       
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contador ++;
+        if (contador > 100)
+        {
+
+            gameObject.GetComponent<Animator>().SetBool("ataque", true);
+            contador = 0;
+        }
+        else {
+            gameObject.GetComponent<Animator>().SetBool("ataque", false);
+        }
+
+
+    }
+}
---
+++ enemigos.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemigos : MonoBehaviour
+{
+    
+    public  int energia { set; get; }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void setAtaque()
+    {
+
+    }
+
+
+    public void setEstadoEnemigo(GameObject go, int ener)
+    {
+        energia = ener;
+        if (energia < 1)
+        {
+            
+            Destroy(go);
+        }
+        
+    }
+    public void setAni_True(GameObject go,string ani)
+    {
+        go.GetComponent<Animator>().SetBool(ani,true); 
+    }
+    public void setAni_False(GameObject go, string ani)
+    {
+        go.GetComponent<Animator>().SetBool(ani, false);
+    }
+
+}
